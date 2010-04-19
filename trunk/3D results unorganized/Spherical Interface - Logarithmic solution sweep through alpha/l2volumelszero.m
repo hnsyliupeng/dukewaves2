d8368e4c7cb37ad9abfaee0258cfe294222ca1a6
dukewaves <dukewaves@7165c486-f8dc-7534-bb64-8857b8ddf1bf>
2010-04-19 14:18:58 +0000

    Undoing our ridiculous dump of data to the repository

--- trunk/3D results unorganized/Spherical Interface - Logarithmic solution sweep through alpha/l2volumelszero.m
+++
@@ -1,7 +0,0 @@
-function [volumep] = l2volumelszero(node,e,x,y,z)
-
-xe = x(node(1:4,e));
-ye = y(node(1:4,e));
-ze = z(node(1:4,e));
-nlink = 4;
-volumep = tetraunitvolume('DUMMY')*parallelipipedvolume3d(xe, ye, ze);
